dee7d58e08da6be3360e919e7257945fa6922973
abdullahanwar1 <120066719+abdullahanwar1@users.noreply.github.com>
2023-10-02 09:55:55 -0600

    week 3 done
    done

--- app/page.js
+++ app/page.js
@@ -8,6 +8,8 @@ export default function Page()
             <h1>CPRG 306: Web Development 2 - Assignments</h1>
            <StudentInfo/>
             <Link href="\week2">week2</Link>
+            <p></p>
+            <Link href="\week3">week3</Link>
         </div>
     )
 }    
